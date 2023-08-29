83e6d9671651d4bd0d953b866a91b87eb38cc308
nemiranda <ne.miranda@alumno.um.edu.ar>
2023-08-29 19:47:25 -0300

    Actualizacion documento scrumbble.

--- game/scrabble.py
+++ game/scrabble.py
@@ -1,5 +1,5 @@
 from game.board import Board
-from game.player import Player
+from game.players import Player
 from game.models import BagTiles
 
 
@@ -7,7 +7,7 @@ class ScrabbleGame:
     def __init__(self, players_count):
         self.board = Board()
         self.bag_tiles = BagTiles()
-        self.player = []
+        self.players = []
         self.dictionary = []
         for _ in range(players_count):
             self.players.append(Player())
