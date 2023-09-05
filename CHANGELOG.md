887e14a49bbb6656efdb4fa70f67d07fe6601a5c
nemiranda <ne.miranda@alumno.um.edu.ar>
2023-09-05 19:58:28 -0300

    Agrego funcion change tiles en player

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 03/09 agrego funcion calculate_word_ value a board, me da error en linea 37 y 50 de test_models, tengo que solucionar
 29/08 mejora de documento base, comienzo changelog y empiezo con test
+05/09 agrego funcion change tiles en player
--- game/players.py
+++ game/players.py
@@ -1,3 +1,16 @@
 class Player:
-    def __init__(self):
+    def __init__(self,name='',number=0,score=0,bag_tiles=None):
+        self.name = name
+        self.number.number
+        self.score=score
         self.tiles = []
+
+    def add_tiles(self,tiles):
+        self.tiles.extend(tiles)
+
+    def change_tiles(self,player_old_tiles_index=[],player_new_tiles=[]):
+        tiles_to_change=[]
+        for tile_index in range (len(player_old_tiles_index)):
+            tiles_to_change.append(self.tiles[player_old_tiles_index[tile_index]-1])
+            self.tiles[player_old_tiles_index[tile_index]-1]=player_new_tiles[player_old_tiles_index[tile_index]-1]
+        return tiles_to_change
